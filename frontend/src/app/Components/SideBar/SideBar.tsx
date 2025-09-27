2f50aad764e010cbf5a30e1b3baad4284ca2274c
Gabriel Diniz <gabrielvmdiniz12@gmail.com>
2025-09-26 22:13:36 -0300

    Adjusting sidebar component

--- frontend/src/app/Components/SideBar/SideBar.tsx
+++ frontend/src/app/Components/SideBar/SideBar.tsx
@@ -1,16 +1,25 @@
+'use client'
+
 import styles from "./style.module.css";
+import Image from "next/image";
+import acleyIcon from "../../assets/sorteador.com.png";
+import { useState } from "react";
 
 export default function SideBar() {
+    const [isOpenMenu, setIsOpenMenu] = useState(false);
+
+    function toggleMenu() {
+        setIsOpenMenu((prev) => !prev);
+    }
     return (
         <>
-            <div className="sideBar">
-                <div className="top">
-
+            <aside className={styles.sideBar}>
+                <div className={styles.top}>
+                    <Image src={acleyIcon} alt="acley logo" className={styles.acleyLogo}/>
+                    <div className={styles.openMenu} onClick={toggleMenu}><div></div></div>
                 </div>
-                <div className={styles.menu}>
-                    
-                </div>
-            </div>
+                <div className={`${styles.menu} ${isOpenMenu ? styles.show : styles.hide}`}></div>
+            </aside>
         </>
     );
 }
