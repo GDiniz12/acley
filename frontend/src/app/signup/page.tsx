73c22da6bf22c4c256db98dccff7c99c4c94a129
Gabriel Diniz <gabrielvmdiniz12@gmail.com>
2025-12-09 11:28:42 -0300

    Fix: route /user in signup

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -15,7 +15,7 @@ export default function SignUp() {
         ev.preventDefault();
 
         try {
-            const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`, {
+            const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
