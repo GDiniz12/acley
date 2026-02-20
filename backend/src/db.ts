f8c0016f49676174479230cf2d94127c7487d08e
Gabriel Diniz <gabrielvmdiniz12@gmail.com>
2026-02-20 11:39:34 -0300

    Fix database connection

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -2,10 +2,10 @@ import mysql from "mysql2/promise"
 import "dotenv/config";
 
 export const pool: mysql.Pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'acley',
-    password: 'Gabigol12345',
+    host: process.env.HOST!,
+    user: process.env.USER!,
+    database: process.env.DB!,
+    password: process.env.PASSWORD!,
     waitForConnections: true,
     connectionLimit: 10
 });
