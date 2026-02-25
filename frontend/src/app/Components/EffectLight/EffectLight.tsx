c2ca50e4616ec28a3642d4c79e05575e667bc392
Gabriel Diniz <gabrielvmdiniz12@gmail.com>
2026-02-25 14:47:01 -0300

    Feature: home page

--- frontend/src/app/Components/EffectLight/EffectLight.tsx
+++
@@ -1,9 +0,0 @@
-import style from "./style.module.css";
-
-export default function EffectLight() {
-    return (
-        <>
-            <div className={style.effectLight}></div>
-        </>
-    )
-}
--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,13 +1,77 @@
-import EffectLight from "../Components/EffectLight/EffectLight";
+"use client";
+
 import styles from "./style.module.css";
 import GlassBox from "../Components/GlassBox/GlassBox";
+import Image from "next/image";
+import acleyLogo from "../assets/acleylogo-remove-background.png"
+import { useEffect, useState } from "react";
+import { getToken } from "../signin/auth";
+
+function whatTimeIs() {
+    const hour = Date().split(" ")[4];
+    if (hour < "12:00:00") {
+        return "Bom dia";
+    }
+    if (hour >= "12:00:00" && hour < "18:00:00") {
+        return "Boa tarde";
+    }
+    return "Boa noite";
+}
+
+interface TypeUser {
+    id: number;
+    username: string;
+    email: string;
+    created_at: string;
+}
 
 export default function HomePage() {
+    const [dataUser, setDataUser] = useState<TypeUser[]>([]);
+    const [finish, setFinish] = useState(false);
+
+    useEffect(() => {
+        async function userDetails() {
+         try {
+          const token = getToken();
+          const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                'Authorization': `Bearer ${token}`
+            }
+          });
+
+          const data: TypeUser[] = await res.json();
+          if (!res.ok) {
+            throw new Error('Erro ao carregar dados do usuário');
+          }
+          console.log(data[0].username)
+          setDataUser(data);
+          setFinish(true);
+         } catch(err) {
+            return err
+         }   
+        }
+
+        userDetails();
+    }, [])
+
     return (
         <>
-        <div className={styles.effects}>
-            <GlassBox width={"25rem"} height={"0.5rem"}/>
-        </div>
+            <div className={styles.container}>
+                <div className={styles.leftSide}>
+                    <span className={styles.logo}>
+                        <Image src={acleyLogo} alt="Acley logo" width={50} height={50}/> 
+                        <h3>Home</h3>    
+                    </span>
+                </div>
+                <div className={styles.main}>
+                    <GlassBox width={"25rem"} height={"0.5rem"}/>
+
+                    <h1>{whatTimeIs()}, {finish ? dataUser[0].username : "No found"}</h1>
+                </div>
+                <div></div>
+            </div>
         </>
     );
 }
