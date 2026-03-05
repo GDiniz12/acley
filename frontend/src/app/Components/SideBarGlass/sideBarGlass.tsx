75d0bfb24f8de8299153c0280e5b9cc08d489bdf
Gabriel Diniz <gabrielvmdiniz12@gmail.com>
2026-03-05 13:37:58 -0300

    Fix glass sidebar

--- frontend/src/app/Components/SideBarGlass/sideBarGlass.tsx
+++ frontend/src/app/Components/SideBarGlass/sideBarGlass.tsx
@@ -1,12 +1,86 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import GlassBox from "../GlassBox/GlassBox";
 import styles from "./style.module.css";
+import { getToken } from "@/app/signin/auth";
+
+interface TypeNotebooks{
+    id: string;
+    name: string;
+}
+
+interface TypeUser {
+    id: number;
+    username: string;
+    created_at: string;
+}
 
 export default function SideBarGlass() {
     const [isOpen, setIsOpen] = useState(false);
+    const [notebooks, setNotebooks] = useState<TypeNotebooks[]>([]);
+    const [dataUser, setDataUser] = useState<TypeUser[]>([]);
 
+    useEffect(() => {
+        async function userData() {
+            try {
+                const token = getToken();
+                const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user`, {
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                });
+
+                if (!res.ok) {
+                    throw new Error('Erro ao carregar dados de usuário');
+                }
+
+                const result: TypeUser[] = await res.json();
+                setDataUser(result);
+            } catch(err) {
+                console.error(err);
+                alert("Erro ao carregar dados do usuário!");
+            }
+        }
+
+        userData();
+    }, []);
+
+    useEffect(() => {
+        const fetchNotebook = async () => {
+            try {
+                const token = getToken();
+                const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/notebook/user/`, {
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                });        
+                if (!res.ok) {
+                    throw new Error('Erro ao carregar notebooks');
+                }
+        
+                const result: TypeNotebooks[] = await res.json();
+                setNotebooks(result);
+            } catch(err) {
+                console.error(err);
+                alert("Erro ao carregar notebooks!");
+            }
+        }
+
+        fetchNotebook();
+    }, []);
+
+    function showNotebooks() {
+        return (
+            <>
+                {notebooks.map((notebook) => (
+                    <div key={notebook.id} className={styles.notebookItem}>
+                        <p className={styles.notebookName}>{notebook.name}</p>
+                    </div>
+                ))}
+            </>
+        )
+    }
     return (
         <>
             <div className={styles.SideBarGlass} onMouseEnter={() => setIsOpen(true)}
@@ -17,7 +91,7 @@ export default function SideBarGlass() {
                         <span>+</span>
                     </div>
                     <div className={styles.allNotebooks}>
-
+                        {showNotebooks()}
                     </div>
                 </GlassBox>
             </div>
